faad0fa82a8b33c3ead1848fd826abf3915101a9
MrMoose111 <wyattreeves1215@gmail.com>
2015-01-04 20:05:01 -0600

    Tested stuff

---
+++ Test.java
@@ -0,0 +1,9 @@
+package BattleCode;
+
+public class Test {
+	
+	public static void main(String[] args){
+		System.out.println("This is a test file to make sure that eclipse is configured with "
+				+ "Github.");
+	}
+}
